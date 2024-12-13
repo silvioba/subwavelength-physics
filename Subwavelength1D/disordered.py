Fixed a lot of bugs in the nonreciprocal capmat calculation and extracted shared disordered logic to DisorderedCommon

Subwavelength1D/disordered.py
@@ -17,72 +17,11 @@ plt.rcParams.update(settings.matplotlib_params)
 
 
 class DisorderedCommon:
-    pass
-
-
-class DisorderedClassicFiniteSWP1D(ClassicFiniteSWP1D):
-    """
-    A class representing a disordered classical finite subwavelength problem in 1D.
-
-    This class extends the `ClassicFiniteSWP1D` class to handle disordered systems of resonators.
-    Attributes:
-        idxs (List[int] | None): Indices indicating the order in which to use the blocks.
-        blocks (List[Tuple[List[int | float]]] | None): Contains the blocks of resonators.
-    Methods:
-        from_finite_wave_problem(fwp):
-            Constructs an instance from a finite wave problem.
-        from_blocks(blocks, idxs, **params):
-            Constructs a finite classical system of disordered blocks of resonators.
-        from_blocks_random(blocks, n_reps, weights=None, seed=42, **params):
-            Constructs a finite classical system of disordered blocks of resonators by randomly picking blocks.
-    """
-
-    def __init__(self, **params):
-        super().__init__(**params)
-        self.idxs = None
-        self.blocks = None
-
-    def __str__(self):
-        return super().__str__()
-
-    @classmethod
-    def from_finite_wave_problem(cls, fwp):
-        """
-        Create an instance of the class from a finite wave problem.
-
-        Args:
-            fwp (FiniteWaveProblem): An instance of FiniteWaveProblem containing the necessary parameters.
-
-        Returns:
-            cls: An instance of the class with parameters initialized from the finite wave problem.
-        """
-        return cls(N=fwp.N, l=fwp.l, s=fwp.s, v_in=fwp.v_in)
-
     @classmethod
     def from_blocks(
         cls, blocks: List[Tuple[List[int | float]]], idxs: List[int], **params
     ) -> Self:
-        """
-        Constructs a finite classical system of disorded blocks of resonators
-
-        Args:
-            blocks (List[List[int  |  float]]): contains the blocks, the inner is given by ([l1,l2,l3], [s1,s2,s3])
-            idxs (List[int]): orders in which to use the blocks
-
-        Returns:
-            Self: DisorderedClassicFiniteSWP1D instance with the specified blocks
-        """
-        l = []
-        s = []
-        for idx in idxs:
-            ll, ss = blocks[idx]
-            l = l + ll
-            s = s + ss
-        s = s[:-1]
-        c = cls(N=len(l), l=np.array(l), s=np.array(s), **params)
-        c.__setattr__("idxs", idxs)
-        c.__setattr__("blocks", blocks)
-        return c
+        raise NotImplementedError
 
     @classmethod
     def from_blocks_random(
@@ -98,7 +37,7 @@ class DisorderedClassicFiniteSWP1D(ClassicFiniteSWP1D):
 
 
         Args:
-            blocks (List[Tuple[List[int  |  float]]]): contains the blocks, the inner is given by ([l1,l2,l3], [s1,s2,s3])
+            blocks (List[Tuple[List[int  |  float]]]): contains the blocks, the inner is given by ([l1,l2,l3], [s1,s2,s3]) or ([l1,l2,...], [s1,s2,...], [g1,g2,...]) depeding on if the system is classic or nonreciprocal
             n_reps (int): total number of blocks in the final structure
             weights (List[float] | None, optional): probabilities to choose the blocks. If none the probabilities are uniform. Defaults to None.
             seed (int, optional): Random seed for reproducibility. Defaults to 42.
@@ -190,5 +129,125 @@ class DisorderedClassicFiniteSWP1D(ClassicFiniteSWP1D):
             left = right
 
 
-class DisorderedNonReciprocalFiniteSWP1D(NonReciprocalFiniteSWP1D):
-    pass
+class DisorderedClassicFiniteSWP1D(DisorderedCommon, ClassicFiniteSWP1D):
+    """
+    A class representing a disordered classical finite subwavelength problem in 1D.
+
+    This class extends the `ClassicFiniteSWP1D` class to handle disordered systems of resonators.
+    Attributes:
+        idxs (List[int] | None): Indices indicating the order in which to use the blocks.
+        blocks (List[Tuple[List[int | float]]] | None): Contains the blocks of resonators.
+    Methods:
+        from_finite_wave_problem(fwp):
+            Constructs an instance from a finite wave problem.
+        from_blocks(blocks, idxs, **params):
+            Constructs a finite classical system of disordered blocks of resonators.
+        from_blocks_random(blocks, n_reps, weights=None, seed=42, **params):
+            Constructs a finite classical system of disordered blocks of resonators by randomly picking blocks.
+    """
+
+    def __init__(self, **params):
+        ClassicFiniteSWP1D.__init__(self, **params)
+        self.idxs = None
+        self.blocks = None
+
+    def __str__(self):
+        return super().__str__()
+
+    @classmethod
+    def from_finite_wave_problem(cls, fwp):
+        """
+        Create an instance of the class from a finite wave problem.
+
+        Args:
+            fwp (FiniteWaveProblem): An instance of FiniteWaveProblem containing the necessary parameters.
+
+        Returns:
+            cls: An instance of the class with parameters initialized from the finite wave problem.
+        """
+        return cls(N=fwp.N, l=fwp.l, s=fwp.s, v_in=fwp.v_in)
+
+    @classmethod
+    def from_blocks(
+        cls, blocks: List[Tuple[List[int | float]]], idxs: List[int], **params
+    ) -> Self:
+        """
+        Constructs a finite classical system of disorded blocks of resonators
+
+        Args:
+            blocks (List[List[int  |  float]]): contains the blocks, the inner is given by ([l1,l2,l3], [s1,s2,s3])
+            idxs (List[int]): orders in which to use the blocks
+
+        Returns:
+            Self: DisorderedClassicFiniteSWP1D instance with the specified blocks
+        """
+        l = []
+        s = []
+        for idx in idxs:
+            assert len(
+                blocks[idx]) == 2, "We expect each block to be a 2-tuple"
+            ll, ss = blocks[idx]
+            l = l + ll
+            s = s + ss
+        s = s[:-1]
+        c = cls(N=len(l), l=np.array(l), s=np.array(s), **params)
+        c.__setattr__("idxs", idxs)
+        c.__setattr__("blocks", blocks)
+        return c
+
+
+class DisorderedNonReciprocalFiniteSWP1D(NonReciprocalFiniteSWP1D, DisorderedCommon):
+    """
+    A class representing a disordered nonreciprocal finite subwavelength problem in 1D.
+
+    This class extends the `NonReciprocalFiniteSWP1D` class to handle disordered systems of nonreciprocal resonators.
+    NOTE: for nonreciprocal systems each block is described by a 3-tuple ([l1,l2,...], [s1,s2,...], [g1,g2,...])
+    Attributes:
+        idxs (List[int] | None): Indices indicating the order in which to use the blocks.
+        blocks (List[Tuple[List[int | float]]] | None): Contains the blocks of resonators.
+    Methods:
+        from_blocks(blocks, idxs, **params):
+            Constructs a finite classical system of disordered blocks of resonators.
+        from_blocks_random(blocks, n_reps, weights=None, seed=42, **params):
+            Constructs a finite classical system of disordered blocks of resonators by randomly picking blocks.
+    """
+
+    def __init__(self, **params):
+        NonReciprocalFiniteSWP1D.__init__(self, **params)
+        self.idxs = None
+        self.blocks = None
+
+    def __str__(self):
+        return super().__str__()
+
+    @classmethod
+    def from_blocks(
+        cls, blocks: List[Tuple[List[int | float]]], idxs: List[int], **params
+    ) -> Self:
+        """
+        Constructs a finite classical system of disorded blocks of resonators
+
+        Args:
+            blocks (List[List[int  |  float]]): contains the blocks, the inner is given by ([l1,l2,...], [s1,s2,...], [g1,g2,...])
+            idxs (List[int]): orders in which to use the blocks
+
+        Returns:
+            Self: DisorderedClassicFiniteSWP1D instance with the specified blocks
+        """
+        l = []
+        s = []
+        g = []
+        for idx in idxs:
+            assert len(
+                blocks[idx]) == 3, "We expect each block to be a 3-tuple"
+            ll, ss, gg = blocks[idx]
+            l = l + ll
+            s = s + ss
+            g = g + gg
+        s = s[:-1]
+        c = cls(
+            N=len(l), l=np.array(l), s=np.array(s), gammas=np.array(g), **params
+        )
+        c.__setattr__("idxs", idxs)
+        c.__setattr__("blocks", blocks)
+        return c

Subwavelength1D/nonreciprocal.py
@@ -39,7 +39,7 @@ class NonReciprocalFiniteSWP1D(FiniteSWP1D):
         super().__init__(**pars)
         if isinstance(gammas, (int, float)):
             gammas = np.ones(self.N) * gammas
-        self.gammas = gammas
+        self.gammas = np.array(gammas, dtype=float)
 
     def __str__(self):
         return super().__str__() + "\nPhysics:      Non-reciprocal system"
@@ -71,7 +71,7 @@ class NonReciprocalFiniteSWP1D(FiniteSWP1D):
         first_coef = self.gammas[0] * self.l[0] / (
             self.s[0] * (1-np.exp(-self.gammas[0]*self.l[0])))
         last_coef = - self.gammas[-1] * self.l[-1] / (
-            self.s[-1]) * (1-np.exp(self.gammas[-1]*self.l[-1]))
+            self.s[-1] * (1-np.exp(self.gammas[-1]*self.l[-1])))
 
         center_diag = self.gammas[1:-1] * self.l[1:-1] * (
             1/(self.s[1:]*(1-np.exp(-self.gammas[1:-1] * self.l[1:-1]))) - 1/(self.s[:-1]*(1-np.exp(self.gammas[1:-1]*self.l[1:-1]))))
@@ -147,19 +147,18 @@ class NonReciprocalPeriodicSWP1D(PeriodicSWP1D):
             Callable[[float], np.ndarray]: map alpha -> C^alpha
         """
 
-        upper_diag = - \
-            self.gammas[:-1] / (self.l[:-1]*self.s) / \
-            (1-np.exp(-self.gammas[:-1]*self.l[:-2]))
-        lower_diag = self.gammas[1:] / (self.l[:1]*self.s) / \
-            (1-np.exp(self.gammas[1:]*self.l[1:-1]))
+        upper_diag = - self.gammas[:-1] * self.l[:-1] / (
+            self.s * (1-np.exp(-self.gammas[:-1]*self.l[:-1])))
+        lower_diag = self.gammas[1:] * self.l[:1] / (
+            self.s * (1-np.exp(self.gammas[1:]*self.l[1:])))
 
-        first_coef = self.gammas[0] * (1/self.s[0]/(1-np.exp(-self.gammas[0] *
-                                                             self.l[0])) - 1/self.s[-1]/(1-np.exp(self.gammas[0]*self.l[0])))
-        last_coef = self.gammas[-1] * (1/self.s[0]/(1-np.exp(-self.gammas[-1] *
-                                                             self.l[-1])) - 1/self.s[-1]/(1-np.exp(self.gammas[-1]*self.l[-1])))
+        first_coef = self.gammas[0] * self.l[0] * (
+            1/(self.s[0]*(1-np.exp(-self.gammas[0] * self.l[0]))) - 1/(self.s[-1]*(1-np.exp(self.gammas[0]*self.l[0]))))
+        last_coef = self.gammas[-1] * self.l[-1] * (
+            1/(self.s[-1]*(1-np.exp(-self.gammas[-1] * self.l[-1]))) - 1/(self.s[-2]*(1-np.exp(self.gammas[-1]*self.l[-1]))))
 
         center_diag = self.gammas[1:-1] * self.l[1:-1] * (
-            1/self.s[1:-1]/(1-np.exp(-self.gammas[1:-1] * self.l[1:-1])) - 1/self.s[:-2]/(1-np.exp(self.gammas[1:-1]*self.l[1:-1])))
+            1/(self.s[1:-1]*(1-np.exp(-self.gammas[1:-1] * self.l[1:-1]))) - 1/(self.s[0:-2]*(1-np.exp(self.gammas[1:-1]*self.l[1:-1]))))
 
         center_diag = np.concatenate([[first_coef], center_diag, [last_coef]])
 
@@ -167,10 +166,10 @@ class NonReciprocalPeriodicSWP1D(PeriodicSWP1D):
             np.diag(lower_diag, -1)
 
         def C(alpha):
-            C0[0, -1] += np.exp(1j * alpha) * (self.gammas[0] / self.s[-1]) * self.l[0] / \
-                (1-np.exp(self.gammas[0]*self.l[0]))
-            C0[-1, 0] += -np.exp(-1j * alpha) * (self.gammas[-1] / self.s[-1]) * self.l[-1] / \
-                (1-np.exp(-self.gammas[-1]*self.l[-1]))
+            C0[0, -1] += np.exp(1j * alpha) * self.gammas[0] * self.l[0] / (
+                self.s[-1] * (1-np.exp(self.gammas[0]*self.l[0])))
+            C0[-1, 0] += -np.exp(-1j * alpha) * self.gammas[-1] * self.l[-1] / (
+                self.s[-1] * (1-np.exp(-self.gammas[-1]*self.l[-1])))
             return C0
 
         return C

Subwavelength1D/swp.py
@@ -230,7 +230,8 @@ class FiniteSWP1D(SWP1D):
             or isinstance(v_in, complex)
         ):
             v_in = (
-                np.ones(N, dtype=complex if isinstance(v_in, complex) else float) * v_in
+                np.ones(N, dtype=complex if isinstance(
+                    v_in, complex) else float) * v_in
             )
 
         if (
@@ -239,7 +240,8 @@ class FiniteSWP1D(SWP1D):
             or isinstance(v_out, complex)
         ):
             v_out = (
-                np.ones(N, dtype=complex if isinstance(v_out, complex) else float)
+                np.ones(N, dtype=complex if isinstance(
+                    v_out, complex) else float)
                 * v_in
             )
 
@@ -355,7 +357,8 @@ class PeriodicSWP1D(SWP1D):
             or isinstance(v_in, complex)
         ):
             v_in = (
-                np.ones(N, dtype=complex if isinstance(v_in, complex) else float) * v_in
+                np.ones(N, dtype=complex if isinstance(
+                    v_in, complex) else float) * v_in
             )
 
         if (
@@ -364,7 +367,8 @@ class PeriodicSWP1D(SWP1D):
             or isinstance(v_out, complex)
         ):
             v_out = (
-                np.ones(N, dtype=complex if isinstance(v_out, complex) else float)
+                np.ones(N, dtype=complex if isinstance(
+                    v_out, complex) else float)
                 * v_in
             )
 