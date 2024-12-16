f0864ef7a20dad1db080e13bf4341c308ea74b51
Silvio <silvio.barandun@sam.math.ethz.ch>
2024-12-16 15:37:16 +0100

    working on qp

--- Subwavelength3D/classic.py
+++ Subwavelength3D/classic.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+from mpmath import polylog
 from Subwavelength3D.swp import SWP3D
 
 import matplotlib.pyplot as plt
@@ -11,10 +13,112 @@ from scipy.special import spherical_jn, hankel1
 from sympy.physics.wigner import wigner_3j
 from scipy.linalg import block_diag
 
+from math import factorial
+
 from joblib import Parallel, delayed  # For parallelism
 
 
 # plt.rcParams.update(settings.matplotlib_params)
+
+
+def lattice_Sn_3D_1D(k, alp, L1x, N_multi):
+    Sn = np.zeros(N_multi + 1, dtype=complex)
+
+    if alp < 0:
+        alp += 2 * np.pi / L1x
+
+    Li = np.zeros((N_multi + 1, 2), dtype=complex)
+
+    # Set precision
+    if alp * L1x < 0.1:
+        mp.dps = 20  # High precision
+    else:
+        mp.dps = 5  # Standard precision
+
+    for l in range(N_multi + 1):
+        z1 = np.exp(1j * (k + alp) * L1x)
+        z2 = np.exp(1j * (k - alp) * L1x)
+        vpa1 = vpa(z1)
+        vpa2 = vpa(z2)
+        Li[l, 0] = polylog(l + 1, vpa1)
+        Li[l, 1] = polylog(l + 1, vpa2)
+
+    for l in range(1, N_multi + 1):
+        summation = 0
+        sign = (-1) ** l
+        factor = 1 / (k * L1x)
+
+        for s in range(l + 1):
+            if s > 1e-6:
+                factor *= (1j * 0.5 * (l + s) * (l - s + 1)) / (s * k * L1x)
+            summation += factor * (Li[s, 0] + sign * Li[s, 1])
+
+        Sn[l] = (-1j) ** (l + 1) * np.sqrt((2 * l + 1) / (4 * np.pi)) * summation
+
+    Sn[0] = (1 / (k * L1x * np.sqrt(4 * np.pi))) * (
+        M(k, alp, L1x) * np.pi
+        - k * L1x
+        + 1j * np.log(2 * abs(np.cos(alp * L1x) - np.cos(k * L1x)))
+    )
+
+    return Sn
+
+
+def M(k, alp, L1x):
+    out = 0
+    beta = alp
+    inc = 2 * np.pi / L1x
+    while beta < k:
+        out += 1
+        beta += inc
+    return out
+
+
+def evaluate_cns(n, s):
+    """
+    Evaluates the expression (n+s)! / (2^s * s! * (n-s)!) efficiently.
+
+    Parameters:
+        n (int): An integer.
+        s (int): An integer.
+
+    Returns:
+        int: The computed result of the expression.
+    """
+    if s < 0 or s > n:
+        raise ValueError("s must satisfy 0 <= s <= n.")
+
+    # Compute factorials
+    fact_n_plus_s = factorial(n + s)
+    fact_s = factorial(s)
+    fact_n_minus_s = factorial(n - s)
+
+    # Compute 2^s
+    two_pow_s = 2**s
+
+    # Final result
+    result = fact_n_plus_s // (two_pow_s * fact_s * fact_n_minus_s)
+
+    return result
+
+
+def Lspm(b, s, pm, k, L):
+    # L_s^{\pm} function in 3.11 in paper
+    return (
+        (1j) ** s / (k * L) ** (s + 1) * polylog(s + 1, np.exp(1j * (k + pm * b) * L))
+    )
+
+
+def lattice_sums(alpha, n, L, k):
+    # Computes lattice sum using 3.10 in paper
+    scaling = np.sqrt((2 * n + 1) / (4 * np.pi)) * (-1j) ** (n + 1)
+    temp = 0
+    for s in range(n + 1):
+        temp += evaluate_cns(n=n, s=s) * (
+            Lspm(b=alpha, s=s, pm=1, k=k, L=L)
+            + (-1) ** n * Lspm(b=alpha, s=s, pm=-1, k=k, L=L)
+        )
+    return scaling * temp
 
 
 def spherical_hl(n, z):
@@ -166,6 +270,34 @@ class ClassicFiniteFWP3D(SWP3D):
     def __str__(self):
         return super().__str__() + "\nPhysics:      Classic system"
 
+    @classmethod
+    def get_SSH(
+        cls, i: int, r: float, s1: float | int, s2: float | int, **params
+    ) -> Self:
+        """ """
+        if i < 1:
+            raise ValueError("i must be a positive integer")
+        if s1 <= 0 or s2 <= 0:
+            raise ValueError("s1 and s2 must be positive")
+
+        centers = []
+        z = 0
+        for k in range(i):
+            centers.append(np.array([0, 0, z]))
+            z += s1 + 2 * r
+            centers.append(np.array([0, 0, z]))
+            z += s2 + 2 * r
+        centers.append(np.array([0, 0, z]))
+        z += s2 + 2 * r
+        for k in range(i):
+            centers.append(np.array([0, 0, z]))
+            z += s1 + 2 * r
+            centers.append(np.array([0, 0, z]))
+            z += s2 + 2 * r
+
+        N = 4 * i + 1
+        return cls(radii=np.ones(N) * r, centers=centers, **params)
+
     def compute_single_layer_potential_matrix_bruteforce(
         self, N_multipole: int
     ) -> np.ndarray:
@@ -193,7 +325,7 @@ class ClassicFiniteFWP3D(SWP3D):
             A = 0
             for lam in range(N_multipole):
                 A += (
-                    np.sqrt((2 * lam + 1) / 4 / np.pi)
+                    np.sqrt((2 * lam + 1) / (4 * np.pi))
                     * C_coefficent(l, m, lp, mp, lam, 0)
                     * spherical_hl(lam, z)
                 )
@@ -233,11 +365,19 @@ class ClassicFiniteFWP3D(SWP3D):
                                         * A_coefficent(
                                             z=self.k0 * rp, l=l, m=m, lp=lp, mp=mp
                                         )
-                                        * spherical_jn(lp, self.k0 * rp)
+                                        * spherical_jn(lp, self.k0 * self.radii[j])
                                         * spherical_jn(l, self.k0 * self.radii[i])
                                     )
                                 else:
                                     if l == lp and m == mp:
+                                        # print(
+                                        #     i,
+                                        #     N_multipole,
+                                        #     l,
+                                        #     m,
+                                        #     "-->",
+                                        #     flat_index(i, N_multipole, l, m),
+                                        # )
                                         S[
                                             flat_index(i, N_multipole, l, m),
                                             flat_index(j, N_multipole, lp, mp),
@@ -346,9 +486,14 @@ class ClassicFiniteFWP3D(SWP3D):
         Q, R = np.linalg.qr(S)
         for j in range(self.N):
             u_j = get_mask_block(N=self.N, N_multi=N_multipole, index=j)
-            y = np.linalg.lstsq(S, u_j, rcond=None)[0]  # np.linalg.solve(R, Q.T @ u_j)
+            y = np.linalg.lstsq(S, u_j, rcond=None)[
+                0
+            ]  # np.linalg.lstsq(S, u_j, rcond=None)[0]  # np.linalg.solve(R, Q.T @ u_j)
+            plt.plot(y)
             for i in range(self.N):
                 u_i = get_mask_block(N=self.N, N_multi=N_multipole, index=i)
+                # print(i, j)
+                # print(u_i.T @ y)
                 C[i, j] = u_i.T @ y * (-4 * np.pi * self.radii[i] ** 2)
         return C
 
@@ -358,32 +503,17 @@ class ClassicFiniteFWP3D(SWP3D):
 
 class ClassicPeriodicFWP3D(SWP3D):
 
-    def __init__(self, **pars):
+    def __init__(self, L, **pars):
+        self.L = L
         super().__init__(**pars)
 
     def __str__(self):
         return super().__str__() + "\nPhysics:      Classic system"
 
-    def compute_lattice_sums(self, N_multipole: int, alpha: float):
-        Q = np.zeros((N_multipole, 1), dtype=complex)
-        Li = np.zeros((N_multipole, 1), dtype=complex)
-        pass
-
     def compute_single_layer_potential_matrix_bruteforce(
         self, N_multipole: int, alpha: float
     ) -> np.ndarray:
-        """
-        Computes the discrete approximation of the single layer potential matrix
-
-        Args:
-            N_multipole (int): Number of multipole to use
-
-        Raises:
-            NotImplementedError: Currently the formula works only for chain of resonators on the z-Axis
-
-        Returns:
-            np.ndarray: a N_multipole**2 * self.N array composed of self.N blocks representing the single layer potential. This is the matrix at the bottom of page 43 [3]
-        """
+        """ """
 
         for c in self.centers:
             if sum(np.abs(c[:2])) > 0:
@@ -406,6 +536,14 @@ class ClassicPeriodicFWP3D(SWP3D):
                 )
             return A
 
+        def B_coef(alpha, l, m, lp, mp):
+            B = 0
+            for lam in range(N_multipole):
+                B += C_coefficent(l, m, lp, mp, lam, 0) * lattice_sums(
+                    alpha, lam, self.L, self.k0
+                )
+            return B
+
         c = -1j * self.radii**2
 
         # For each l there are 2l+1 Y^l_m functions. Summing up we get to the following number
@@ -425,38 +563,59 @@ class ClassicPeriodicFWP3D(SWP3D):
             for j in range(self.N):
                 for l in range(N_multipole):
                     for m in range(-l, l + 1):
-                        for lp in range(N_multipole):
-                            for mp in range(-lp, lp + 1):
-                                if i != j:
-                                    rp = np.linalg.norm(
-                                        self.centers[i] - self.centers[j]
-                                    )
+                        if i == j:
+                            S[
+                                flat_index(i, N_multipole, l, m),
+                                flat_index(j, N_multipole, l, m),
+                            ] = (
+                                c[i]
+                                * self.k0
+                                * spherical_hl(l, self.radii[i] * self.k0)
+                                * spherical_jn(l, self.radii[i] * self.k0)
+                            )
+                            for lp in range(N_multipole):
+                                for mp in range(-lp, lp + 1):
+                                    # print(B_coef(alpha, l, m, lp, mp))
                                     S[
                                         flat_index(i, N_multipole, l, m),
-                                        flat_index(j, N_multipole, lp, mp),
+                                        flat_index(j, N_multipole, l, m),
+                                    ] += (
+                                        B_coef(alpha, l, m, lp, mp)
+                                        * spherical_jn(lp, self.k0 * self.radii[i])
+                                        * c[i]
+                                        * self.k0
+                                        * spherical_jn(l, self.k0 * self.radii[i])
+                                    )
+                        else:
+                            rp = np.linalg.norm(self.centers[i] - self.centers[j])
+                            for lpp in range(N_multipole):
+                                for mpp in range(-lpp, lpp + 1):
+                                    temp = 0
+                                    for lp in range(N_multipole):
+                                        for mp in range(-lp, lp + 1):
+                                            for lam in range(N_multipole):
+                                                temp += (
+                                                    B_coef(alpha, l, m, lp, mp)
+                                                    * C_coefficent(
+                                                        lp, mp, lpp, mpp, lam, 0
+                                                    )
+                                                    * spherical_jn(lam, self.k0 * rp)
+                                                )
+                                    S[
+                                        flat_index(i, N_multipole, l, m),
+                                        flat_index(j, N_multipole, lpp, mpp),
                                     ] = (
                                         c[i]
                                         * self.k0
-                                        * A_coefficent(
-                                            z=self.k0 * rp, l=l, m=m, lp=lp, mp=mp
-                                        )
-                                        * spherical_jn(lp, self.k0 * rp)
+                                        * spherical_jn(lpp, self.k0 * rp)
                                         * spherical_jn(l, self.k0 * self.radii[i])
+                                        * temp
                                     )
-                                else:
-                                    if l == lp and m == mp:
-                                        S[
-                                            flat_index(i, N_multipole, l, m),
-                                            flat_index(j, N_multipole, lp, mp),
-                                        ] = (
-                                            c[i]
-                                            * self.k0
-                                            * spherical_hl(l, self.radii[i] * self.k0)
-                                            * spherical_jn(l, self.radii[i] * self.k0)
-                                        )
         return S
 
-    def compute_single_layer_potential_matrix(self, N_multipole: int) -> np.ndarray:
+    def compute_single_layer_potential_matrix(
+        self, alpha, N_multipole: int
+    ) -> np.ndarray:
         """
         Computes the discrete approximation of the single layer potential matrix
 
@@ -542,9 +701,9 @@ class ClassicPeriodicFWP3D(SWP3D):
 
         return S
 
-    def get_capacitance_matrix(self, N_multipole=1) -> np.ndarray:
+    def get_capacitance_matrix(self, alpha, N_multipole=1) -> np.ndarray:
         S = self.compute_single_layer_potential_matrix_bruteforce(
-            N_multipole=N_multipole
+            alpha=alpha, N_multipole=N_multipole
         )
         C = np.zeros((self.N, self.N), dtype=complex)
 
